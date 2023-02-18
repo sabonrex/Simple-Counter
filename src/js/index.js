59973ac67b523bd458b0b2ced4b434fe3156b63b
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-02-18 18:16:50 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,6 @@ import "../styles/index.css";
 import App from "./component/home.jsx";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
+
+// cuatro o 5 components const for each digit)componentete sueltos, invidual 
+// 1 var contantdo, vuelve a 0 (contador del anterior (por su digito))
