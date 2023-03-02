9bea72f84bf02781e777feed2558d3532e57069f
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-03-02 20:29:19 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,26 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter(counter => counter + 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return <h1>{counter}</h1>;
+//create your first component
+const Home = () => {
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button... bootstrap is working...
+			</a>
+			<p>
+				Made by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				love!
+			</p>
+		</div>
+	);
 };
 
-export default App;
+export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import Proptypes from "prop-types"
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -8,6 +9,28 @@ import "../styles/index.css";
 //import your own components
 import App from "./component/home.jsx";
 
+function SimpleCounter(prop){
+    return(<div className="bigCounter">
+        <div className="four">{prop.digitFour}</div>
+        <div className="three">{prop.digitThree}</div>
+        <div className="two">{prop.digitTwo}</div>
+        <div className="one">{prop.digitOne}</div>
+    </div>);
+}
+
+SimpleCounter.protoType = {
+    digitFour: protoType.number,
+    digitThree: protoType.number,
+    digitTwo: protoType.number,
+    digitOne: protoType.number,
+};
+let counter = 0;
+setInterval (function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+})
 ReactDOM.render(<App/>, document.getElementById("root"));
 
 // cuatro o 5 components const for each digit)componentete sueltos, invidual 
